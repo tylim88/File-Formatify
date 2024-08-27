8606134182aeb93a240d030dd24531b40f2b4817
tylim <limteikyee@gmail.com>
2024-08-28 00:12:21 +0800

    fix loader issue

--- src/component/FileList.tsx
+++ src/component/FileList.tsx
@@ -117,7 +117,7 @@ export const FileList = () => {
 											<Text truncate>{sizeText}</Text>
 										</Table.Td>
 										<Table.Td>
-											{status !== 'processing' ? (
+											{status === 'processing' ? (
 												<Center>
 													<Loader size="xs" color="white" />
 												</Center>
